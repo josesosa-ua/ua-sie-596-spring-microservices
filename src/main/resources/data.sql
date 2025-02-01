60226e7bdbcd5a40900d328ef964633c0b1d308b
Jose Sosa <josesosa@arizona.edu>
2025-01-31 23:12:20 -0500

    Added one more test organization and one person that presides two organizations to show the one to many relation.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,8 @@ INSERT INTO person (id, name, major, dept, date_of_birth, phone, email) VALUES
 -- Insert data into the Organization table
 INSERT INTO organization (id, name, category, established_date, president_id) VALUES
 (1, 'Tennis Club', 'Sports', '2010-09-01', 3),
-(2, 'Poetry Club', 'Arts', '2012-01-15', 4);
+(2, 'Poetry Club', 'Literature', '2012-01-15', 4),
+(3, 'Theater Club', 'Arts', '2012-01-15', 3);
 
 -- Insert data into the person_organization join table
 INSERT INTO person_organization (organization_id, person_id) VALUES
@@ -17,4 +18,7 @@ INSERT INTO person_organization (organization_id, person_id) VALUES
 (1, 3),
 (2, 1),
 (2, 3),
-(2, 4);
+(2, 4),
+(3, 2),
+(3, 3),
+(3, 4);
