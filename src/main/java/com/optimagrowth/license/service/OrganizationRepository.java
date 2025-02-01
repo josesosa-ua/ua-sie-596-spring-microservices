33eeac7912e9e801677cc97a9f2f06e9c5441641
Jose Sosa <josesosa@arizona.edu>
2025-01-31 21:25:51 -0500

    Minor code formatting.

--- src/main/java/com/optimagrowth/license/service/OrganizationRepository.java
+++ src/main/java/com/optimagrowth/license/service/OrganizationRepository.java
@@ -1,16 +1,16 @@
 package com.optimagrowth.license.service;
 
-import com.optimagrowth.license.model.Organization;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.List;
+import com.optimagrowth.license.model.Organization;
 
 @RepositoryRestResource()
 public interface OrganizationRepository
     extends PagingAndSortingRepository<Organization, Long>, CrudRepository<Organization, Long> {
-
   List<Organization> findByName(@Param("name") String name);
 }
