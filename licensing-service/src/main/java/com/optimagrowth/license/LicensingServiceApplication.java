283122c92d6ad9775798f352f18006c70e6241d2
Jose Sosa <josesosa@arizona.edu>
2025-03-09 22:24:12 -0400

    Config working, but only native.

--- licensing-service/src/main/java/com/optimagrowth/license/LicensingServiceApplication.java
+++ licensing-service/src/main/java/com/optimagrowth/license/LicensingServiceApplication.java
@@ -3,12 +3,15 @@ package com.optimagrowth.license;
 import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
+@RefreshScope
 public class LicensingServiceApplication {
 
     public static void main(String[] args) {
@@ -16,13 +19,16 @@ public class LicensingServiceApplication {
     }
 
     @Bean
-    public LocaleResolver localeResolver() {
-        var localeResolver = new SessionLocaleResolver();
+    @Primary // Optional: Makes your bean the primary choice if autowired
+    public LocaleResolver customLocaleResolver() { // Changed method name from
+                                                   // localeResolver
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
         localeResolver.setDefaultLocale(Locale.US);
         return localeResolver;
     }
 
     @Bean
+    @Primary
     public ResourceBundleMessageSource messageSource() {
         var messageSource = new ResourceBundleMessageSource();
         messageSource.setUseCodeAsDefaultMessage(true);
