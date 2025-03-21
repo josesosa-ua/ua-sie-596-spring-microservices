6cb2a1d020ff0b1e6b8ce8aba5fefc1d0f688035
Jose Sosa <josesosa@arizona.edu>
2025-03-21 09:23:44 -0400

    Got chapter 5 postgres to work. Need to add some code.

--- licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -3,11 +3,16 @@ package com.optimagrowth.license.controller;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.optimagrowth.license.model.License;
 import com.optimagrowth.license.service.LicenseService;
@@ -19,7 +24,7 @@ public class LicenseController {
     @Autowired
     private LicenseService licenseService;
 
-    @GetMapping(value = "/{licenseId}")
+    @RequestMapping(value = "/{licenseId}", method = RequestMethod.GET)
     public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId,
             @PathVariable("licenseId") String licenseId) {
 
@@ -27,32 +32,35 @@ public class LicenseController {
         license.add(
                 linkTo(methodOn(LicenseController.class).getLicense(organizationId, license.getLicenseId()))
                     .withSelfRel(),
-                linkTo(methodOn(LicenseController.class).createLicense(organizationId, license, null))
-                    .withRel("createLicense"),
-                linkTo(methodOn(LicenseController.class).updateLicense(organizationId, license, null))
-                    .withRel("updateLicense"),
-                linkTo(methodOn(LicenseController.class).deleteLicense(organizationId, license.getLicenseId(), null))
+                linkTo(methodOn(LicenseController.class).createLicense(license)).withRel("createLicense"),
+                linkTo(methodOn(LicenseController.class).updateLicense(license)).withRel("updateLicense"),
+                linkTo(methodOn(LicenseController.class).deleteLicense(license.getLicenseId()))
                     .withRel("deleteLicense"));
 
         return ResponseEntity.ok(license);
     }
 
     @PutMapping
-    public ResponseEntity<String> updateLicense(@PathVariable("organizationId") String organizationId,
-            @RequestBody License request, @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
-        return ResponseEntity.ok(licenseService.updateLicense(request, organizationId, locale));
+    public ResponseEntity<License> updateLicense(@RequestBody License request) {
+        return ResponseEntity.ok(licenseService.updateLicense(request));
     }
 
     @PostMapping
-    public ResponseEntity<String> createLicense(@PathVariable("organizationId") String organizationId,
-            @RequestBody License request, @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
-        return ResponseEntity.ok(licenseService.createLicense(request, organizationId, locale));
+    public ResponseEntity<License> createLicense(@RequestBody License request) {
+        License license = licenseService.createLicense(request);
+        license.add(linkTo(methodOn(LicenseController.class).createLicense(request)).withSelfRel(),
+                linkTo(methodOn(LicenseController.class).getLicense(license.getOrganizationId(),
+                        license.getLicenseId()))
+                    .withRel("getLicense"),
+                linkTo(methodOn(LicenseController.class).updateLicense(request)).withRel("updateLicense"),
+                linkTo(methodOn(LicenseController.class).deleteLicense(request.getLicenseId()))
+                    .withRel("deleteLicense"));
+        return ResponseEntity.ok(license);
     }
 
     @DeleteMapping(value = "/{licenseId}")
-    public ResponseEntity<String> deleteLicense(@PathVariable("organizationId") String organizationId,
-            @PathVariable("licenseId") String licenseId, @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
-        return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId, locale));
+    public ResponseEntity<String> deleteLicense(@PathVariable("licenseId") String licenseId) {
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId));
     }
 
 }
--- licensing-service/src/test/java/com/optimagrowth/license/LicenseServiceApplicationTests.java
+++ licensing-service/src/test/java/com/optimagrowth/license/LicenseServiceApplicationTests.java
@@ -1,9 +1,12 @@
 package com.optimagrowth.license;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 class LicenseServiceApplicationTests {
 
     @Test
