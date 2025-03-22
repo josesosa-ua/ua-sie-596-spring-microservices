6772d88d272aead6860bde694886eff832b61015
Jose Sosa <josesosa@arizona.edu>
2025-03-22 13:17:11 -0400

    Got chapter 6 simple single eureka working. Will build PostMan collection next.

---
+++ eureka-server/src/test/java/com/optimagrowth/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.optimagrowth.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
